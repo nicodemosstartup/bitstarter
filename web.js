611b7db46d0e6e73508882320fd9fd3c8d533572
Nicodemos Protopapas <nicodemos.startup@gmail.com>
2013-07-31 11:57:26 +0000

    Fixed index.html & web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+ var buf = new Buffer(fs.readFileSync('index.html'));
+       response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
